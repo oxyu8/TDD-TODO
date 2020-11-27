6aeb23fd35c44f67323418912ef4790fe38b4f5e
oxyu8 <liberalist0449@gmail.com>
2020-11-27 18:35:03 +0900

    add firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
 import {PageTitle} from '../components/PageTitle'
 import { HelloButton } from '../components/HelloButton'
+import { db } from '../../firebaseConfig'
 
 
 const Home = () => {
+  const addTodo = async() => {
+    db.collection('todo').doc('test').set({
+      name: 'test'
+    })
+    .then(() => {
+      console.log('success')
+    })
+    .catch((error) => {
+      console.log(error)
+      console.error('error', error)
+    })
+
+  }
   return (
     <>
       <PageTitle title={'welcome to tokyo'} color={'red'}></PageTitle>
-      <HelloButton text={'click me'} handleFunc = {() => console.log('hello')}></HelloButton>
+      <HelloButton text={'click me'} handleFunc = {addTodo}></HelloButton>
     </>
   )
 }
